08f8c284a74731fd01efcfb7d547c9244ff76fda
Ryo <7890bask@gmail.com>
2022-01-25 23:41:17 +0900

    :sparkles: mypageaxios設定

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -8,10 +8,10 @@
         <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">質問事項一覧</h1>
       </div>
       <div class="flex flex-wrap -m-4">
-        <div class="p-4 md:w-1/3">
+        <div class="p-4 md:w-1/3" v-for="question in questions" :key=question.id>
           <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
             <div class="flex items-center mb-3">
-              <h2 class="text-gray-900 text-lg title-font font-medium">質問名前</h2>
+              <h2 class="text-gray-900 text-lg title-font font-medium">{{question.title}}</h2>
             </div>
             <div class="flex-grow">
               <p class="leading-relaxed text-base">テストテストテストテストテストテストテストテストテストテストテストテスト</p>
@@ -107,6 +107,7 @@
 
 <script>
 import Vue from 'vue'
+import axios from 'axios'
 export default Vue.extend({
   name: 'MyPage',
   // middleware: [ 'auth', userAuth ],
@@ -115,5 +116,14 @@ export default Vue.extend({
       return this.$auth.loggedIn ? this.$auth.$state.user.nickname : 'ゲスト'
     },
   },
+  data() {
+    return {
+      questions: [],
+    }
+  },
+  mounted() {
+    axios.get('/api/index')
+     .then((response) => this.questions = response.data)
+  }
 })
 </script>
