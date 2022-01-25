8b137943be336654175f73efe7ae19c72d40f722
Ryo <7890bask@gmail.com>
2022-01-25 23:47:56 +0900

    :sparkles: Mypagedata取得表示

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -14,83 +14,8 @@
               <h2 class="text-gray-900 text-lg title-font font-medium">{{question.title}}</h2>
             </div>
             <div class="flex-grow">
-              <p class="leading-relaxed text-base">テストテストテストテストテストテストテストテストテストテストテストテスト</p>
+              <p class="leading-relaxed text-base">{{question.answer}}</p>
               <a class="mt-3 text-indigo-500 inline-flex items-center" href="/detail">もっと見る
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="p-4 md:w-1/3">
-          <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-            <div class="flex items-center mb-3">
-              <h2 class="text-gray-900 text-lg title-font font-medium">質問名前</h2>
-            </div>
-            <div class="flex-grow">
-              <p class="leading-relaxed text-base">テストテストテストテストテストテストテストテストテストテストテストテスト</p>
-              <a class="mt-3 text-indigo-500 inline-flex items-center" href="/detail">もっと見る
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="p-4 md:w-1/3">
-          <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-            <div class="flex items-center mb-3">
-              <h2 class="text-gray-900 text-lg title-font font-medium">質問名前</h2>
-            </div>
-            <div class="flex-grow">
-              <p class="leading-relaxed text-base">テストテストテストテストテストテストテストテストテストテストテストテスト</p>
-              <a class="mt-3 text-indigo-500 inline-flex items-center">もっと見る
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="p-4 md:w-1/3">
-          <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-            <div class="flex items-center mb-3">
-              <h2 class="text-gray-900 text-lg title-font font-medium">質問名前</h2>
-            </div>
-            <div class="flex-grow">
-              <p class="leading-relaxed text-base">テストテストテストテストテストテストテストテストテストテストテストテスト</p>
-              <a class="mt-3 text-indigo-500 inline-flex items-center">もっと見る
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="p-4 md:w-1/3">
-          <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-            <div class="flex items-center mb-3">
-              <h2 class="text-gray-900 text-lg title-font font-medium">質問名前</h2>
-            </div>
-            <div class="flex-grow">
-              <p class="leading-relaxed text-base">テストテストテストテストテストテストテストテストテストテストテストテスト</p>
-              <a class="mt-3 text-indigo-500 inline-flex items-center">もっと見る
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="p-4 md:w-1/3">
-          <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-            <div class="flex items-center mb-3">
-              <h2 class="text-gray-900 text-lg title-font font-medium">質問名前</h2>
-            </div>
-            <div class="flex-grow">
-              <p class="leading-relaxed text-base">テストテストテストテストテストテストテストテストテストテストテストテスト</p>
-              <a class="mt-3 text-indigo-500 inline-flex items-center">もっと見る
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
