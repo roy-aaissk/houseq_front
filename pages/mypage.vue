c1f5921df63b2226b5083124f892b7010499bcf9
Ryo <7890bask@gmail.com>
2022-01-26 00:48:24 +0900

    :recycle: mypagedetailデータ取得表示

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -15,11 +15,12 @@
             </div>
             <div class="flex-grow">
               <p class="leading-relaxed text-base">{{question.answer}}</p>
-              <a class="mt-3 text-indigo-500 inline-flex items-center" href="/detail">もっと見る
+              <div>
+                <NuxtLink class="mt-3 text-indigo-500 inline-flex items-center" :to="`/detail/${question.id}`">もっと見る</NuxtLink>
+              </div>
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
-              </a>
             </div>
           </div>
         </div>
