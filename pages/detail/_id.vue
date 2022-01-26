0503bf5604213793e3026c0946831441a84442bc
Ryo <7890bask@gmail.com>
2022-01-27 00:32:25 +0900

    :recycle: Editbutton作成db取得

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -22,6 +22,7 @@
             質問タグ
           </dt>
           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            {{questions.tag}}
           </dd>
         </div>
         <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -29,6 +30,7 @@
             回答
           </dt>
           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            {{questions.answer}}
           </dd>
         </div>
         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -48,6 +50,13 @@
           </dd>
         </div>
       </dl>
+  </div>
+  <div class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+    <NuxtLink to="../edit">
+      <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+        Edit
+      </button>
+    </NuxtLink>
   </div>
   <Footer/>
 </div>
--- pages/mypage.vue
+++ pages/mypage.vue
@@ -16,11 +16,12 @@
             <div class="flex-grow">
               <p class="leading-relaxed text-base">{{question.answer}}</p>
               <div>
-                <NuxtLink class="mt-3 text-indigo-500 inline-flex items-center" :to="{ path: `/detail/${question.id}`}">もっと見る</NuxtLink>
-              </div>
+                <NuxtLink class="mt-3 text-indigo-500 inline-flex items-center" :to="{ path: `/detail/${question.id}`}">もっと見る
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
+                </NuxtLink>
+              </div>
             </div>
           </div>
         </div>
