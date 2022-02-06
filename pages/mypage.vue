7b59d642422b8039679195055575cf56a5f34eef
Ryo <7890bask@gmail.com>
2022-02-07 05:55:42 +0900

    :adhesive_bandage: data表示条件分岐

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -11,12 +11,12 @@
         <div class="p-4 md:w-1/3" v-for="question in questions" :key=question.id>
           <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
             <div class="flex items-center mb-3">
-              <h2 class="text-gray-900 text-lg title-font font-medium" v-if="question.title">{{question.title}}</h2>
+              <h2 class="text-gray-900 text-lg title-font font-medium" v-if="question.title" >{{question.title}}</h2>
             </div>
             <div class="flex-grow">
-              <p class="leading-relaxed text-base">{{question.answer}}</p>
+              <p class="leading-relaxed text-base" v-if="question.answer">{{question.answer}}</p>
               <div>
-                <NuxtLink class="mt-3 text-indigo-500 inline-flex items-center" :to="{ path: `/detail/${question.id}`}">もっと見る
+                <NuxtLink class="mt-3 text-indigo-500 inline-flex items-center" :to="{ path: `/detail/${question.id}` }">もっと見る
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
